4f79cccae300e832b5d3a665ce5bb28e7e29003f
Gideon Onita <onitagideon30@gmail.com>
2025-06-10 19:40:32 +0100

    header scroll function added

--- src/components/headers/AppHeader.tsx
+++ src/components/headers/AppHeader.tsx
@@ -4,9 +4,32 @@ import { Button } from '../ui/button'
 import ModeToggle from '../theme/mode-toggle'
 import { SidebarTrigger } from '../ui/sidebar'
 import { nonUserNavLinks } from '@/utils/data'
+import { useEffect, useState } from 'react'
 function AppHeader() {
+  const [showHeader, setShowHeader] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+
+      if (currentScrollY > 50 && currentScrollY > lastScrollY) {
+        setShowHeader(false)
+      } else {
+        setShowHeader(true)
+      }
+      setLastScrollY(currentScrollY)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [lastScrollY])
   return (
-    <header className="bg-navbarbg/80 backdrop-blur-md shadow-sm sticky top-0 z-50 py-3">
+    <header
+      className={`bg-navbarbg/80 backdrop-blur-md shadow-sm sticky w-full top-0 z-50 py-3 ${
+        showHeader ? 'translate-y-0' : '-translate-y-full'
+      }`}
+    >
       <div className="container flex items-center justify-between">
         <Logo icon="w-5 h-5" text="text-sm" />
         <div className="hidden md:flex items-center gap-x-8">
