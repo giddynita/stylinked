819d4354ed4ef29f52c8f4449a4adc039b2ba416
Gideon Onita <onitagideon30@gmail.com>
2025-08-06 16:42:42 +0100

    accessibility improvement in footer

--- src/components/global/AppFooter.tsx
+++ src/components/global/AppFooter.tsx
@@ -23,18 +23,20 @@ function AppFooter() {
   const footerLinks = user ? authUsersLinks : nonUserFooterLinks
 
   return (
-    <footer className="bg-navbarbg text-accent-foreground pt-12 pb-6">
+    <footer className="bg-navbarbg/70 pt-12 pb-6">
       <div className=" container grid md:grid-cols-4 gap-8">
         <div>
-          <Logo icon="w-5 h-5" text="text-sm" />
-          <p className="text-accent-foreground text-xs font-medium mt-2 max-w-[12rem]">
+          <Logo icon="w-6 h-6" text="text-base" />
+          <p className=" text-xs font-medium mt-2 max-w-[12rem]">
             Connecting fashion enthusiasts with skilled artisans worldwide.
           </p>
         </div>
         {footerLinks.map((group, index) => {
           return (
-            <section key={index}>
-              <h3 className="font-semibold text-sm mb-4">{group.heading}</h3>
+            <div key={index}>
+              <span className="font-semibold text-sm mb-4">
+                {group.heading}
+              </span>
               <ul>
                 {group.links.map((link, index) => {
                   return (
@@ -49,7 +51,7 @@ function AppFooter() {
                   )
                 })}
               </ul>
-            </section>
+            </div>
           )
         })}
       </div>
