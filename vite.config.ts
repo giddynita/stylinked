e17ef88d6ed8973045ff64fc450f3d63d48448e0
Gideon Onita <onitagideon30@gmail.com>
2025-08-08 00:59:21 +0100

    another change

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
       output: {
         manualChunks: {
           // Core React
-          react: ['react', 'react-dom'],
-          routing: ['react-router-dom', 'react-lazyload'],
+          reactdom: ['react-dom'],
+          react: ['react', 'react-router-dom', 'react-lazyload'],
           reactQuery: ['@tanstack/react-query'],
 
           // State & Form
