f718b519bd1edab915246e862eb588b971c908aa
Gideon Onita <onitagideon30@gmail.com>
2025-08-07 21:21:08 +0100

    reversal

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,8 @@ export default defineConfig({
       output: {
         manualChunks: {
           // Core React
-          react: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          lazyloading: ['react-lazyload'],
+          react: ['react', 'react-dom', 'react-router-dom', 'react-lazyload'],
+
           // State & Form
           state: ['@reduxjs/toolkit', 'react-redux'],
           query: ['@tanstack/react-query'],
