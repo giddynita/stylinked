c158ae5cc155d2efd0a45fc5aefa30f3e604c191
Gideon Onita <onitagideon30@gmail.com>
2025-08-07 02:37:29 +0100

    removed a dev dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
