71eb30efbaee66a33fa3948eb15c4a0a8337f0d9
Gideon Onita <onitagideon30@gmail.com>
2025-08-08 14:21:47 +0100

    code split

--- src/components/formTypes/LoginForm.tsx
+++ src/components/formTypes/LoginForm.tsx
@@ -1,22 +1,22 @@
 import { GlobalContext } from '@/utils/globalContext'
 import { loginFormSchema, type LoginFormSchema } from '@/utils/schema'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext, useState } from 'react'
+import { lazy, useContext, useState } from 'react'
 
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { Card, CardContent } from '../ui/card'
 import { AuthFormsHeading } from '../headings'
-import FormInput from '../form/FormInput'
-import FormPassword from '../form/FormPassword'
-import SubmitButton from '../form/SubmitButton'
 import { Button } from '../ui/button'
 import { login } from '@/utils/api'
 import { useDispatch } from 'react-redux'
 import { setUser } from '@/features/user/userSlice'
 import { Form } from '../ui/form'
 import { useForm } from 'react-hook-form'
-import LazyLoad from 'react-lazyload'
+import { nullSuspense } from '@/utils/suspense'
+const FormInput = lazy(() => import('../form/FormInput'))
+const FormPassword = lazy(() => import('../form/FormPassword'))
+const SubmitButton = lazy(() => import('../form/SubmitButton'))
 
 function LoginForm() {
   const navigate = useNavigate()
@@ -54,45 +54,50 @@ function LoginForm() {
       <CardContent>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormInput
-              form={form}
-              label="Email"
-              placeholder="Enter your email address"
-              type="email"
-              name="email"
-            />
-            <FormPassword
-              form={form}
-              name="password"
-              label="Password"
-              placeholder="Enter your password"
-            />
-            <SubmitButton
-              submitting={isPending}
-              text="Login"
-              texting="Logging in"
-              /* setSubmitting={setIsPending} */
-            />
+            {nullSuspense(
+              <FormInput
+                form={form}
+                label="Email"
+                placeholder="Enter your email address"
+                type="email"
+                name="email"
+              />
+            )}
+            {nullSuspense(
+              <FormPassword
+                form={form}
+                name="password"
+                label="Password"
+                placeholder="Enter your password"
+              />
+            )}
+
+            {nullSuspense(
+              <SubmitButton
+                submitting={isPending}
+                text="Login"
+                texting="Logging in"
+                /* setSubmitting={setIsPending} */
+              />
+            )}
           </form>
         </Form>
-        <LazyLoad>
-          <p className="text-center pt-2  text-sm font-medium text-foreground ">
-            Don't have an account?
-            <Button asChild={true} variant="link">
-              <Link to="/auth/sign-up" className="text-primary -ml-2">
-                Sign up
-              </Link>
-            </Button>
-          </p>
-          <p className="text-center -mt-4  text-sm font-medium text-foreground ">
-            Forgot password?
-            <Button asChild={true} variant="link">
-              <Link to="/auth/forgot-password" className="text-primary -ml-2">
-                Reset password
-              </Link>
-            </Button>
-          </p>
-        </LazyLoad>
+        <p className="text-center pt-2  text-sm font-medium text-foreground ">
+          Don't have an account?
+          <Button asChild={true} variant="link">
+            <Link to="/auth/sign-up" className="text-primary -ml-2">
+              Sign up
+            </Link>
+          </Button>
+        </p>
+        <p className="text-center -mt-4  text-sm font-medium text-foreground ">
+          Forgot password?
+          <Button asChild={true} variant="link">
+            <Link to="/auth/forgot-password" className="text-primary -ml-2">
+              Reset password
+            </Link>
+          </Button>
+        </p>
       </CardContent>
     </Card>
   )
