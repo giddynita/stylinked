62dfc56aae175e874db43d2666aad9a805384cfc
Gideon Onita <onitagideon30@gmail.com>
2025-08-26 12:55:38 +0100

    fix:code splitting for cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,29 +1,30 @@
-import { CartItems, EmptyCart, OrderSummary } from '@/components/cart'
+import { CartItems, EmptyCart } from '@/components/cart'
 import AppHeader from '@/components/headers/AppHeader'
 import { PageHeading } from '@/components/headings'
+import { sectionSuspense } from '@/utils/suspense'
+import { lazy } from 'react'
 import { useSelector } from 'react-redux'
+
+const OrderSummary = lazy(() => import('@/components/cart/OrderSummary'))
 
 function Cart() {
   const { numItemsInCart } = useSelector((state: any) => state.cartState)
 
   return (
     <>
+      <PageHeading pageDesc="Cart items" pageTitle="Cart" />
       <AppHeader />
       <main
         className={`min-h-screen container space-y-18 my-12 ${
           numItemsInCart && 'lg:grid-cols-3'
         }  lg:grid gap-10`}
       >
-        <PageHeading pageDesc="Cart items" pageTitle="Cart" />
-
         <section className="col-span-2">
           {!numItemsInCart ? <EmptyCart /> : <CartItems />}
         </section>
-        {!numItemsInCart || (
-          <section>
-            <OrderSummary />
-          </section>
-        )}
+        <section>
+          {!numItemsInCart || sectionSuspense(<OrderSummary />)}
+        </section>
       </main>
     </>
   )
