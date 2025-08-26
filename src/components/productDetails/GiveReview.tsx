f22172e8f490d2e9dea24db64129430aee2003bc
Gideon Onita <onitagideon30@gmail.com>
2025-08-26 06:21:36 +0100

    fix:code splitting for product details page

--- src/components/productDetails/GiveReview.tsx
+++ src/components/productDetails/GiveReview.tsx
@@ -1,11 +1,13 @@
 import { Card, CardContent } from '../ui/card'
 import type { SingleProduct } from '@/utils/types'
-import { ReviewForm } from '../formTypes'
 import { useSelector } from 'react-redux'
+import { lazy } from 'react'
+import { formSuspense } from '@/utils/suspense'
 
 interface GiveReviewProp {
   product: SingleProduct | undefined
 }
+const ReviewForm = lazy(() => import('@/components/formTypes/ReviewForm'))
 
 function GiveReview({ product }: GiveReviewProp) {
   const { user } = useSelector((state: any) => state.userState)
@@ -17,7 +19,7 @@ function GiveReview({ product }: GiveReviewProp) {
           <h2 className="text-xl font-bold mb-4">Write a Review</h2>
           <Card>
             <CardContent>
-              <ReviewForm product={product} />
+              {formSuspense(<ReviewForm product={product} />)}
             </CardContent>
           </Card>
         </section>
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -3,14 +3,17 @@ import { Cart } from '@/components/marketplace'
 import { useSingleProduct } from '@/utils/hooks'
 import { ProductInfoSkeleton } from '@/components/skeletons'
 import SubPagesHeader from '@/components/headers/SubPagesHeader'
-import { FetchingError, ProductReviews } from '@/components/global'
-
+import { FetchingError } from '@/components/global'
 import { PageHeading } from '@/components/headings'
 import {
   GiveReview,
   ProductImages,
   ProductInfo,
 } from '@/components/productDetails'
+import { lazy } from 'react'
+import { sectionSuspense } from '@/utils/suspense'
+
+const ProductReviews = lazy(() => import('@/components/global/ProductReviews'))
 
 const ProductDetails = () => {
   const { productid } = useParams()
@@ -24,14 +27,13 @@ const ProductDetails = () => {
 
   return (
     <div className="min-h-screen relative container">
-      {/*breadcrumbs */}
       <SubPagesHeader currentPage={product?.name} previousPage="marketplace" />
-      {/* page title */}
+
       <PageHeading
         pageTitle={`Product - ${product?.name}`}
         pageDesc={product?.description}
       />
-      {/* product details */}
+
       {productLoading ? (
         <ProductInfoSkeleton />
       ) : (
@@ -45,15 +47,16 @@ const ProductDetails = () => {
           </div>
           {/* Reviews Section */}
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 mb-12">
-            {/* Existing Reviews */}
             <section>
-              <ProductReviews reviews={product?.productReviews} />
+              {sectionSuspense(
+                <ProductReviews reviews={product?.productReviews} />
+              )}
             </section>
 
             {/* Write a Review */}
             <GiveReview product={product} />
           </div>
-          {/* cart */}
+
           <div className="border bg-background shadow-xs hover:bg-accent group hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 cursor-pointer fixed w-14 h-14 sm:h-18 sm:w-18 rounded-full flex items-center justify-center top-1/3 -translate-y-1/3 right-4">
             <Cart />
           </div>
