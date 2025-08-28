7079ae5a095e9928c393d79488791ea749217819
Gideon Onita <onitagideon30@gmail.com>
2025-08-28 05:46:25 +0100

    fix:adjust testimonial carousel styling

--- src/components/global/TestimonialCarousel.tsx
+++ src/components/global/TestimonialCarousel.tsx
@@ -15,18 +15,21 @@ function TestimonialCarousel({ testimonials }: TestimonialCarouselProp) {
               key={index}
               className="carousel-item  bg-primary/10 border-none mt-12 rounded-md"
             >
-              <div className="p-6">
-                <div className="flex items-center space-x-1 mb-4">
-                  {[...Array(rating)].map((_, i) => (
-                    <Star
-                      key={i}
-                      className="w-5 h-5 fill-yellow-400 text-yellow-400"
-                    />
-                  ))}
+              <div className="flex flex-col justify-between gap-4 p-6">
+                <div>
+                  <div className="flex items-center space-x-1 mb-4">
+                    {[...Array(rating)].map((_, i) => (
+                      <Star
+                        key={i}
+                        className="w-5 h-5 fill-yellow-400 text-yellow-400"
+                      />
+                    ))}
+                  </div>
+                  <p className="text-foreground max-w-[12rem] line-clamp-6">
+                    "{text}"
+                  </p>
                 </div>
-                <p className="text-foreground mb-4 max-w-[12rem] h-32 line-clamp-6">
-                  "{text}"
-                </p>
+
                 <div>
                   <p className="text-primary font-semibold ">{name}</p>
                   <p className="text-sm text-muted-foreground">{role}</p>
